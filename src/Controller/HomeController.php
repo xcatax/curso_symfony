b874d7a05a44de50a003a07c20f835d454f73a1a
Catalina <rattocatalina@gmail.com>
2021-05-14 12:37:30 -0300

    ejercicio 2

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+/**
+* @Route("/home")
+* 
+*/
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/principal", name="home")
+     */    
+    public function index()
+    {           
+        $titulos = array();
+        for ($i = 1; $i <= 20; $i++){
+           array_push($titulos,"Titulo");
+        } 
+     
+        return $this->render('home/index.html.twig', ['titulos' => $titulos]);	           
+        
+        
+    }
+}
