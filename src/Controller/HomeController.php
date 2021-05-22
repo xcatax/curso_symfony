51e917008f86f70534fd285a2fff521ce479b8ed
Catalina <rattocatalina@gmail.com>
2021-05-22 15:46:02 -0300

    agregando usuarios

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-
+use App\Entity\User;
 
 /**
 * @Route("/home")
@@ -28,5 +28,25 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', ['titulos' => $titulos]);	           
         
         
+    }
+    /**
+     * @Route("/principal", name="home")
+     */
+
+    public function newUser(Request $request){
+
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to your action: index(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = new User("Catalina","12345");
+       
+        // tell Doctrine you want to (eventually) save the User (no queries yet)
+        $entityManager->persist($user);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+        
+        return new Response('Saved new product with id '.$user->getId());
     }
 }
